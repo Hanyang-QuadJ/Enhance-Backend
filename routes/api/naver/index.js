4b5dc6ac849a803323b4cd05ef4f4ed623d7129e
Yoon Jee Woo <yoonjeewoo@gmail.com>
2018-05-01 20:29:29 +0900

    형 미안~

--- routes/api/naver/index.js
+++ routes/api/naver/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const controller = require('./naver.controller');
 
-router.get('/search/news/:coin_id/:source', controller.naverNewsSearch);
+router.get('/search/news', controller.naverNewsSearch);
 
 module.exports = router;
--- routes/api/naver/naver.controller.js
+++ routes/api/naver/naver.controller.js
@@ -5,13 +5,13 @@ const config = require('../../../config');
 const conn = mysql.createConnection(config);
 
 exports.naverNewsSearch = (req, res) => {
-    const { coin_id, source } = req.params;
     conn.query(
-        'SELECT * FROM News WHERE (coin_id = ? and source = ?)',
-        [coin_id, source],
+        `SELECT * FROM News WHERE (coin_id = ? and source = ?) LIMIT 10 OFFSET ${req.query.index}`,
+        [req.query.coin_id, req.query.source],
         (err, result) => {
             if(err) throw err;
             return res.status(200).json({
+                nextIndex: parseInt(req.query.index) + 10,
                 result
             })
         }
