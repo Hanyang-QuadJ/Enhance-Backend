d9fb0ec7934a982bc492ca3d45b1fd7ebb90f985
JeongHyunLee <nomar0107@gmail.com>
2018-05-01 14:05:59 +0900

    test

--- routes/api/naver/naver.controller.js
+++ routes/api/naver/naver.controller.js
@@ -13,7 +13,7 @@ exports.naverNewsSearch = (req, res) => {
      headers: {'X-Naver-Client-Id':config.client_id, 'X-Naver-Client-Secret': config.client_secret}
   };
   request.get(options, function (error, response, body) {
-   if (!error && response.statusCode == 200) {
+   if (!error && response.statusCode === 200) {
      res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
      res.end(body);
    } else {
