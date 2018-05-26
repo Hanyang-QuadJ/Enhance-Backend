dd3b818a00debf858a1aed479b1aa0102eb5a27d
Yoon Jee Woo <yoonjeewoo@gmail.com>
2018-05-26 18:21:09 +0900

    블로그 뉴스 최신순 정렬

--- routes/api/naver/naver.controller.js
+++ routes/api/naver/naver.controller.js
@@ -5,32 +5,62 @@ const config = require('../../../config');
 const conn = mysql.createConnection(config);
 
 exports.naverSearch = (req, res) => {
-    if(req.query.source === encodeURI(0)){
-        conn.query(
-            `SELECT * FROM News WHERE (coin_id = ?) LIMIT 30 OFFSET ${req.query.index}`,
-            [req.query.coin_id],
-            (err, result) => {
-                if(err) throw err;
-                return res.status(200).json({
-                    nextIndex: parseInt(req.query.index) + 30,
-                    result
-                })
-            }
-        )
+    if (req.query.recent == 0) {
+        if (req.query.source === encodeURI(0)) {
+            conn.query(
+                `SELECT * FROM News WHERE (coin_id = ?) ORDER BY STR_TO_DATE(pubDate, '%a, %d %M %Y %H:%i:%s') DESC LIMIT 30 OFFSET ${req.query.index}`,
+                [req.query.coin_id],
+                (err, result) => {
+                    if (err) throw err;
+                    return res.status(200).json({
+                        nextIndex: parseInt(req.query.index) + 30,
+                        result
+                    })
+                }
+            )
+        }
+        else if (req.query.source === encodeURI(1)) {
+            conn.query(
+                `SELECT * FROM Blogs WHERE (coin_id = ?) ORDER BY pubDate DESC LIMIT 30 OFFSET ${req.query.index}`,
+                [req.query.coin_id],
+                (err, result) => {
+                    if (err) throw err;
+                    return res.status(200).json({
+                        nextIndex: parseInt(req.query.index) + 30,
+                        result
+                    })
+                }
+            )
+        }
+    } else {
+        if (req.query.source === encodeURI(0)) {
+            conn.query(
+                `SELECT * FROM News WHERE (coin_id = ?) LIMIT 30 OFFSET ${req.query.index}`,
+                [req.query.coin_id],
+                (err, result) => {
+                    if (err) throw err;
+                    return res.status(200).json({
+                        nextIndex: parseInt(req.query.index) + 30,
+                        result
+                    })
+                }
+            )
+        }
+        else if (req.query.source === encodeURI(1)) {
+            conn.query(
+                `SELECT * FROM Blogs WHERE (coin_id = ?) LIMIT 30 OFFSET ${req.query.index}`,
+                [req.query.coin_id],
+                (err, result) => {
+                    if (err) throw err;
+                    return res.status(200).json({
+                        nextIndex: parseInt(req.query.index) + 30,
+                        result
+                    })
+                }
+            )
+        }
     }
-    else if(req.query.source === encodeURI(1)){
-        conn.query(
-            `SELECT * FROM Blogs WHERE (coin_id = ?) LIMIT 30 OFFSET ${req.query.index}`,
-            [req.query.coin_id],
-            (err, result) => {
-                if(err) throw err;
-                return res.status(200).json({
-                    nextIndex: parseInt(req.query.index) + 30,
-                    result
-                })
-            }
-        )
-    }
+    
 
 };
 
