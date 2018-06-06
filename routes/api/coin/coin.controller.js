482308eee119d6f72a88fd493aed2c173c960f1a
Yoon Jee Woo <yoonjeewoo@gmail.com>
2018-06-06 15:50:58 +0900

    error 500

--- routes/api/coin/coin.controller.js
+++ routes/api/coin/coin.controller.js
@@ -5,7 +5,7 @@ const conn = mysql.createConnection(config);
 
 exports.getAllCoin = (req, res) => {
     conn.query('SELECT * from Coins', (err, coins) => {
-        if (err) throw err;
+        if (err) return res.status(500).json({ err });
         return res.status(200).json({
             message: 'get every coin successfully',
             coins: coins
