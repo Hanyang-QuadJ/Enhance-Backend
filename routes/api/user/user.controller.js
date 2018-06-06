57812e5cc8fd2a863f4cf0126fcacc2226e22fbf
Yoon Jee Woo <yoonjeewoo@gmail.com>
2018-06-06 14:57:23 +0900

    유저네임 변경시, 이메일 유저네임 중복체크

--- routes/api/user/user.controller.js
+++ routes/api/user/user.controller.js
@@ -93,16 +93,30 @@ exports.getUserById = (req, res) => {
 
 exports.updateUsername = (req, res) => {
     const {username, email} = req.body;
-    console.log(req.decoded._id);
+    // console.log(req.decoded._id);
     conn.query(
-        `UPDATE Users SET username = '${username}' ,email = '${email}' WHERE id = ${req.decoded._id}`,
+        "SELECT * FROM Users WHERE username = ? or email = ?",
+        [username, email],
         (err, result) => {
-            if (err) throw err;
-            return res.status(200).json({
-                message: "success"
-            })
+            if (err) return res.status(500).json({ err });
+            if (result.length == 0) {
+                conn.query(
+                    `UPDATE Users SET username = '${username}' ,email = '${email}' WHERE id = ${req.decoded._id}`,
+                    (err, result) => {
+                        if (err) throw err;
+                        return res.status(200).json({
+                            message: "success"
+                        })
+                    }
+                )
+            } else {
+                return res.status(406).json({
+                    message: '이메일 혹은 유저네임이 이미 존재합니다.'
+                })
+            }
         }
     )
+    
 };
 
 exports.changeProfileImage = (req, res) => {
